c8c03b3051b4cb2f06ea4c9e7ec985947a1dd81a
niludarekar1 <nilu.darekar1@gmail.com>
2022-05-03 18:38:08 +0530

    Modified:Weges Per Day

--- src/com/bridgelabz/EmployeeWages.java
+++ src/com/bridgelabz/EmployeeWages.java
@@ -6,7 +6,8 @@ public class EmployeeWages {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int WAGE_PER_HOUR = 20;
-    public static final int WORKING_DAY_MONTH = 20;
+    public static final int NUM_WORKING_DAYS = 20;
+    public static final int MAX_HOUR_IN_MONTH = 100;
 
     public static void main(String[] args) {
 
@@ -17,8 +18,12 @@ public class EmployeeWages {
         int empWage = 0;
         int empHrs = 0;
         int totalEmpWage = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
 
-        for( int i=1; i <= WORKING_DAY_MONTH; i++ ) {
+        while( totalEmpHrs <= MAX_HOUR_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS) {
+
+            totalWorkingDays++;     //Incrementing Working Days
 
             //Computation
             double empCheck = Math.floor(Math.random() * 10) % 3;
@@ -37,9 +42,11 @@ public class EmployeeWages {
                     empHrs = 0;
                     break;
             }
+            totalEmpHrs += empHrs;      //Calculating Total Hours
             empWage = WAGE_PER_HOUR * empHrs;   // Calculating Employee Wage if Employee is Present as PART TIME Or FULL TIME
             totalEmpWage = empWage + totalEmpWage;
             System.out.println("Employee Wage = " + empWage);
+            System.out.println("Days:"+totalWorkingDays+"\tTotal Hours:"+totalEmpHrs);
         }
 
         //Displaying Total Emp Wages
