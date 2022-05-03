7d7c25cf32d072cb404db99e72a814a104be3eea
niludarekar1 <nilu.darekar1@gmail.com>
2022-05-03 18:31:08 +0530

    Added:Part Time Weges

--- src/com/bridgelabz/EmployeeWages.java
+++ src/com/bridgelabz/EmployeeWages.java
@@ -7,22 +7,29 @@ public class EmployeeWages {
         System.out.println("Welcome to Employee Wages Problem Developed by Nilesh Darekar.");
 
         //Constants
-        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
         int WAGE_PER_HOUR = 20;
-        int FULL_DAY_HOUR = 8;
 
         //Variable for Calculating Employee Wage
         int empWage = 0;
+        int empHrs = 0;
 
         //Computation
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
 
-        if(empCheck == IS_FULL_TIME) {
-            System.out.println("Employee is Present");
-            empWage = WAGE_PER_HOUR * FULL_DAY_HOUR ;   // Calculating Employee Wage if Employee is Present
-            System.out.println("Employee Wage = " + empWage);
+        if(empCheck == IS_PART_TIME) {
+            System.out.println("Employee is Present as PART TIME");
+            empHrs = 4;
+        } else if(empCheck == IS_FULL_TIME){
+            System.out.println("Employee is Present as FULL TIME");
+            empHrs = 8;
         } else {
             System.out.println("Employee is Absent");
+            empHrs = 0;
         }
+
+        empWage = WAGE_PER_HOUR * empHrs ;   // Calculating Employee Wage if Employee is Present as PART TIME Or FULL TIME
+        System.out.println("Employee Wage = " + empWage);
     }
 }
