049969c75dafccfd94f77bf7383d4125aa13864d
niludarekar1 <nilu.darekar1@gmail.com>
2022-05-03 18:33:25 +0530

    Added:Using Switch Case

--- src/com/bridgelabz/EmployeeWages.java
+++ src/com/bridgelabz/EmployeeWages.java
@@ -1,15 +1,16 @@
 package com.bridgelabz;
 
 public class EmployeeWages {
+
+    //Constants
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int WAGE_PER_HOUR = 20;
+
     public static void main(String[] args) {
 
         //Welcome Message for Initial Purpose
         System.out.println("Welcome to Employee Wages Problem Developed by Nilesh Darekar.");
-
-        //Constants
-        int IS_PART_TIME = 1;
-        int IS_FULL_TIME = 2;
-        int WAGE_PER_HOUR = 20;
 
         //Variable for Calculating Employee Wage
         int empWage = 0;
@@ -18,15 +19,19 @@ public class EmployeeWages {
         //Computation
         double empCheck = Math.floor(Math.random() * 10) % 3;
 
-        if(empCheck == IS_PART_TIME) {
-            System.out.println("Employee is Present as PART TIME");
-            empHrs = 4;
-        } else if(empCheck == IS_FULL_TIME){
-            System.out.println("Employee is Present as FULL TIME");
-            empHrs = 8;
-        } else {
-            System.out.println("Employee is Absent");
-            empHrs = 0;
+        switch ((int) empCheck) {
+            case IS_PART_TIME :
+                System.out.println("Employee is Present as PART TIME");
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME :
+                System.out.println("Employee is Present as FULL TIME");
+                empHrs = 8;
+                break;
+            default :
+                System.out.println("Employee is Absent");
+                empHrs = 0;
+                break;
         }
 
         empWage = WAGE_PER_HOUR * empHrs ;   // Calculating Employee Wage if Employee is Present as PART TIME Or FULL TIME
